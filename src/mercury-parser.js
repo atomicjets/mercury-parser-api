a73cd6249043c7a3f604f2531a3604dcc7b68009
Atomicjets <webmaster@atomicjets.com>
2020-01-27 12:36:31 +0000

    Update contenttype and serverless region

--- src/mercury-parser.js
+++ src/mercury-parser.js
@@ -5,7 +5,11 @@ import { corsSuccessResponse, corsErrorResponse, runWarm } from './utils';
 const mercuryParser = async ({ queryStringParameters }, context, cb) => {
   const { url } = queryStringParameters;
 
-  const result = await Mercury.parse(url);
+  const result = await Mercury.parse(url, {
+    contentType: 'markdown',
+    'User-Agent':
+      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
+  });
 
   return cb(
     null,
